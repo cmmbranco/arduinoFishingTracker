0b2b224663301a4761eac08f60b6379e3c083ae9
cmmbranco <carlos.m.branco@tecnico.ulisboa.pt>
2021-11-04 20:18:16 +0000

    fixes arduino readable format

--- fishing_tracker/fishing_tracker.ino
+++ fishing_tracker/fishing_tracker.ino
@@ -4,6 +4,7 @@
 #include <SD.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <avr/sleep.h>
 
 // Time Control vars
 #define GPSWAIT 10000
@@ -13,17 +14,20 @@
 //Other vars
 #define LED A0
 #define LEDGPS A2
-#define BUTTON A1
-uint8_t BUTTON_STATE = LOW;
-uint8_t PREV_BUTTON_STATE = LOW;
+#define REGISTER_BUTTON A1
+#define WAKE_BUTTON 2
+uint8_t WAKE_BUTTON_STATE = HIGH;
+uint8_t PREV_WAKE_BUTTON_STATE = HIGH;
+uint8_t REGISTER_BUTTON_STATE = LOW;
+uint8_t PREV_REGISTER_BUTTON_STATE = LOW;
 #define percent 100
 
 // Fat limitation: filename (8 chars name) + "." (1 char) + file extension (3 chars)
 #define filename "fishdata.csv"
 
 // The TinyGPS++ object
-#define rxPin 2
-#define txPin 3
+#define rxPin 5
+#define txPin 6
 float lati = 0.00, longi = 0.00, last_lat = 0.00, last_long = 0.00;
 TinyGPSPlus gps;
 
@@ -52,10 +56,11 @@ void setup(){
   // Setup pins
   pinMode(LED, OUTPUT);
   pinMode(LEDGPS, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(REGISTER_BUTTON, INPUT_PULLUP);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  BUTTON_STATE = digitalRead(BUTTON);
+  digitalWrite (WAKE_BUTTON, HIGH);  // enable pull-up
+  REGISTER_BUTTON_STATE = digitalRead(REGISTER_BUTTON);
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -89,6 +94,7 @@ void setup(){
 void fetchAndStore(){
   digitalWrite(LED,HIGH); // Turn on led to inform processing
   
+  
   // Get GPS data
   unsigned long start = millis();
   do{
@@ -117,29 +123,84 @@ void fetchAndStore(){
   if (myFile) {
     myFile.print(lati); myFile.print(','); myFile.print(longi); myFile.print(',');
     myFile.print(air_temp); myFile.print(',');myFile.print(air_pressure); myFile.print(',');
-    myFile.print(water_temp); myFile.print(',');
+    myFile.println(water_temp);
     
     // close the file:
     myFile.close();
-    Serial.println(F("done."));
   }
   
   digitalWrite(LED,LOW); 
+  digitalWrite(LEDGPS, LOW);
 }
 
+void wake ()
+{
+  // cancel sleep as a precaution
+  sleep_disable();
+  // precautionary while we do other stuff
+  detachInterrupt (0);
+}  // end of wake
+
 void loop(){
+  digitalWrite(LED, HIGH);
+  delay(50);
+  digitalWrite(LED, LOW);
+  delay(50);
+
+  
 
   if(gps.satellites.value() > 3 && gps.satellites.isValid()){
     digitalWrite(LEDGPS,HIGH); // Turn on led to inform gps available
-  } else {digitalWrite(LEDGPS,LOW);}
+  } else {digitalWrite(LEDGPS,HIGH); delay(50); digitalWrite(LEDGPS,LOW);}
   
-  BUTTON_STATE = digitalRead(BUTTON);
-  Serial.println(BUTTON_STATE);
-  if ( BUTTON_STATE != PREV_BUTTON_STATE){
+  REGISTER_BUTTON_STATE = digitalRead(REGISTER_BUTTON);
+  //Serial.println(REGISTER_BUTTON_STATE);
+  if ( REGISTER_BUTTON_STATE != PREV_REGISTER_BUTTON_STATE){
     // has the button switch been closed?
-    if ( BUTTON_STATE == LOW ){
+    if ( REGISTER_BUTTON_STATE == LOW ){
       fetchAndStore();
     }
   }
-  PREV_BUTTON_STATE = BUTTON_STATE;
+  
+  PREV_REGISTER_BUTTON_STATE = REGISTER_BUTTON_STATE;
+
+  
+  PREV_WAKE_BUTTON_STATE = HIGH;
+  WAKE_BUTTON_STATE = HIGH;
+  
+  WAKE_BUTTON_STATE = digitalRead(WAKE_BUTTON);
+  //Serial.println(WAKE_BUTTON_STATE);
+  if ( WAKE_BUTTON_STATE != PREV_WAKE_BUTTON_STATE){
+    // has the button switch been closed?
+    if ( WAKE_BUTTON_STATE == LOW ){
+      //Serial.println("HERE");
+      // disable ADC
+      ADCSRA = 0;
+      
+      set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+      sleep_enable();
+    
+      // Do not interrupt before we go to sleep, or the
+      // ISR will detach interrupts and we won't wake.
+      noInterrupts ();
+      
+      // will be called when pin D2 goes low  
+      attachInterrupt (0, wake, FALLING);
+      EIFR = bit (INTF0);  // clear flag for interrupt 0
+     
+      // turn off brown-out enable in software
+      // BODS must be set to one and BODSE must be set to zero within four clock cycles
+      MCUCR = bit (BODS) | bit (BODSE);
+      // The BODS bit is automatically cleared after three clock cycles
+      MCUCR = bit (BODS); 
+      
+      // We are guaranteed that the sleep_cpu call will be done
+      // as the processor executes the next instruction after
+      // interrupts are turned on.
+      interrupts ();  // one cycle
+      sleep_cpu ();   // one cycle
+      delay(150);
+    }
+  }
+    
 } 
